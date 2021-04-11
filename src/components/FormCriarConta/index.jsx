f88070c06cc43fec460f268848eedc032b8a2c41
kainanpinheiro <kainan.psilva@gmail.com>
2021-04-11 19:16:55 -0300

    Integracao do cadastro de conta

--- src/components/FormCriarConta/index.jsx
+++ src/components/FormCriarConta/index.jsx
@@ -1,121 +1,143 @@
 import React, { useState } from 'react';
 import Recaptcha from 'react-recaptcha';
+import { useHistory } from 'react-router-dom';
 
+import api from '../../services/api';
 
 import './style.css';
 
 function FormCriarConta(props) {
-    const [login, setLogin] = useState("");
-    const [nome, setNome] = useState("");
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [senhaConfirmada, setSenhaConfirmada] = useState("");
+  const history = useHistory();
 
-    
-    const [recaptchaVerificado, setRecaptchaVerificado] = useState(false);
+  const [login, setLogin] = useState('');
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [senhaConfirmada, setSenhaConfirmada] = useState('');
 
-    const onloadCallback = () => {
-        console.log("recaptcha foi carregado");
-    };
+  const [recaptchaVerificado, setRecaptchaVerificado] = useState(false);
 
-    const verifyCallback = (response) => {
-        if(response) {
-            setRecaptchaVerificado(true);
-        }
+  const onloadCallback = () => {
+    console.log('recaptcha foi carregado');
+  };
+
+  const verifyCallback = (response) => {
+    if (response) {
+      setRecaptchaVerificado(true);
+    }
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (senha !== senhaConfirmada) {
+      alert('As senhas não conferem!');
     }
 
-    return (
-        <section className="container .container-sm .container-md .container-lg .container-xl .container-xxl">
-            <div className="card text-white mb-3 mt-5">
-                <form
-                    onSubmit={(event) => {
-                        event.preventDefault();
-                    }}
-                    className="card-body"
-                    id="form-agricultor"
-                >
-                    <div className="mb-3">
-                        <input
-                            value={login}
-                            onChange={(event) => {
-                                setLogin(event.target.value);
-                            }}
-                            type="text"
-                            className="form-control"
-                            id="login"
-                            placeholder="Login"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            value={nome}
-                            onChange={(event) => {
-                                setNome(event.target.value)
-                            }}
-                            type="text"
-                            className="form-control"
-                            id="nome"
-                            placeholder="Nome"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            value={email}
-                            onChange={(event) => {
-                                setEmail(event.target.value)
-                            }}
-                            type="email"
-                            className="form-control"
-                            id="email"
-                            placeholder="E-mail"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            value={senha}
-                            onChange={(event) => {
-                                setSenha(event.target.value);
-                            }}
-                            type="password"
-                            className="form-control"
-                            id="senha"
-                            placeholder="Senha"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            value={senhaConfirmada}
-                            onChange={(event) => {
-                                setSenhaConfirmada(event.target.value)
-                            }}
-                            type="password"
-                            className="form-control"
-                            id="confirmaSenha"
-                            placeholder="Confirmar Senha" />
-                    </div>
-                    <div className="mb-3 formCriarContaRecaptcha">
-                        <Recaptcha
-                            sitekey="6LepmaUaAAAAAO2a852MonsGvQ1HVVhxSzm2qINy"
-                            render="explicit"
-                            verifyCallback={verifyCallback}
-                            onloadCallback={onloadCallback}
-                        />
-                        <input
-                            onClick={() => {
-                                if (recaptchaVerificado) {
-                                    alert("Você está verificado!")
-                                } else {
-                                    alert("Pare, prove que você não é um robo!")
-                                }
-                            }}
-                            className="formCriarContaSubmit g-recaptcha"
-                            type="submit"
-                            value="Criar" />
-                    </div>
-                </form>
-            </div>
-        </section>
-    );
+    if (!recaptchaVerificado) {
+      alert('Pare, prove que você não é um robo!');
+    }
+
+    await api.post('agricultor', {
+      login,
+      nome,
+      email,
+      senha,
+    });
+
+    history.push('/');
+  }
+
+  return (
+    <section className="container .container-sm .container-md .container-lg .container-xl .container-xxl">
+      <div className="card text-white mb-3 mt-5">
+        <form
+          onSubmit={handleSubmit}
+          className="card-body"
+          id="form-agricultor"
+        >
+          <div className="mb-3">
+            <input
+              value={login}
+              onChange={(event) => {
+                setLogin(event.target.value);
+              }}
+              type="text"
+              className="form-control"
+              id="login"
+              placeholder="Login"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              value={nome}
+              onChange={(event) => {
+                setNome(event.target.value);
+              }}
+              type="text"
+              className="form-control"
+              id="nome"
+              placeholder="Nome"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="E-mail"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              value={senha}
+              onChange={(event) => {
+                setSenha(event.target.value);
+              }}
+              type="password"
+              className="form-control"
+              id="senha"
+              placeholder="Senha"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              value={senhaConfirmada}
+              onChange={(event) => {
+                setSenhaConfirmada(event.target.value);
+              }}
+              type="password"
+              className="form-control"
+              id="confirmaSenha"
+              placeholder="Confirmar Senha"
+              required
+            />
+          </div>
+          <div className="mb-3 formCriarContaRecaptcha">
+            <Recaptcha
+              sitekey="6LepmaUaAAAAAO2a852MonsGvQ1HVVhxSzm2qINy"
+              render="explicit"
+              verifyCallback={verifyCallback}
+              onloadCallback={onloadCallback}
+            />
+            <input
+              className="formCriarContaSubmit g-recaptcha"
+              type="submit"
+              value="Criar"
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default FormCriarConta;
