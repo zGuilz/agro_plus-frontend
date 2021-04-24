923921aa9437ea092fdbd68829fb0aff06372849
Andre Pereira Prado <1900986@alunoimpacta.com.br>
2021-04-24 13:42:42 -0300

    cria estados para form cliente login

--- src/pages/LoginCliente/index.jsx
+++ src/pages/LoginCliente/index.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 import './style.css';
 
 
 function LoginCliente() {
+    const [login, setLogin] = useState('');
+    const [userPassword, setUserPassword] = useState('');
 
+    async function handleSubmit(event) {
+        event.preventDefault();
+    }
 
     return (
         <section className="loginCliente">
@@ -14,11 +19,30 @@ function LoginCliente() {
                 <section className="loginCliente_header">
                     <h2>Cliente Login</h2>
                 </section>
-                <form className="loginCliente_form loginCliente_input">
-                   
-                    <input type="text" className="loginCliente_input"/>
-                    <input type="password" className="loginCliente_input"/>
-                    <input type="submit" className="loginCliente_submit loginCliente_input" value="Sign In"/>
+                <form
+                    className="loginCliente_form loginCliente_input"
+                    onSubmit={handleSubmit}
+                >
+
+                    <input
+                        value={login}
+                        onChange={(event) => {
+                            setLogin(event.target.value);
+                        }}
+                        type="text"
+                        className="loginCliente_input"
+                        placeholder="Login"
+                    />
+                    <input
+                        value={userPassword}
+                        onChange={(event) => {
+                            setUserPassword(event.target.value);
+                        }}
+                        type="password"
+                        className="loginCliente_input"
+                        placeholder="Senha"
+                    />
+                    <input type="submit" className="loginCliente_submit loginCliente_input" value="Sign In" />
                 </form>
             </section>
         </section>
