c8e6e931eeff3ddb45bb7eca72d9200ca1c1dbe9
kainanpinheiro <kainan.psilva@gmail.com>
2021-05-05 21:45:57 -0300

    Botao ir para calculadora

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,23 @@
 import React, { Fragment } from 'react';
 import Header from '../../components/Header';
+import Button from '../../components/Button';
 
 import logoEmpresa from '../../assets/logoHome.png';
-
 
 import './style.css';
 
 function Home() {
-    return(
-        <Fragment>
-            <Header />
-            <section className="home">
-                <section className="home_logoEmpresa">
-                    <img src={logoEmpresa} alt="logo da empresa Agro Mais"/>
-                </section>
-            </section>
-        </Fragment>
-    ); 
+  return (
+    <Fragment>
+      <Header />
+      <section className="home">
+        <section className="home_logoEmpresa">
+          <img src={logoEmpresa} alt="logo da empresa Agro Mais" />
+        </section>
+        <Button to="/calculadora" text="Calculadora" />
+      </section>
+    </Fragment>
+  );
 }
 
 export default Home;
